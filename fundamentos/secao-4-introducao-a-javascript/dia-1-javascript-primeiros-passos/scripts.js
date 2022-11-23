0b397347f952f2b48ab8e161e6bdf3ee2a8db568
Carolina Matos <carolinantunes.matos@gmail.com>
2022-11-23 16:12:38 -0300

    treinando js

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Carolina";
+let birthCity = "Natal";
+let birthYear = 1995;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+birthCity = "Natal";
+
+console.log(birthCity);
+console.log(birthYear);
